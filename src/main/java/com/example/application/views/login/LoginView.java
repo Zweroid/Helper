db9b1c08b8aad175477f91be3e5a9d1261a6fe77
zweroid <zweroid@mail.ru>
2025-02-21 16:52:10 +0300

    PassAndLoginDesing

--- src/main/java/com/example/application/views/login/LoginView.java
+++ src/main/java/com/example/application/views/login/LoginView.java
@@ -3,6 +3,7 @@ package com.example.application.views.login;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
@@ -18,11 +19,31 @@ public class LoginView extends VerticalLayout implements BeforeEnterObserver {
     private final LoginForm loginForm;
 
     public LoginView() {
+
+        LoginI18n i18n = LoginI18n.createDefault();
+
+        LoginI18n.Form i18nForm = i18n.getForm();
+        i18nForm.setTitle("Авторизация");
+        i18nForm.setUsername("Логин");
+        i18nForm.setPassword("Пароль");
+        i18nForm.setSubmit("Вход");
+
+        i18n.setForm(i18nForm);
+
+        LoginI18n.ErrorMessage i18nErrorMessage = i18n.getErrorMessage();
+        i18nErrorMessage.setTitle("Неверное имя пользователя или пароль");
+        i18nErrorMessage.setMessage(
+                "Проверьте правильность имени пользователя и пароля и повторите попытку.");
+        i18n.setErrorMessage(i18nErrorMessage);
+
+
         loginForm = new LoginForm();
         setSizeFull();
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
 
+
+        loginForm.setI18n(i18n);
         loginForm.setAction("login");
 
         add(loginForm);
